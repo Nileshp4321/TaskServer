2ce7244e39f47f6dfdecb8ced9432f0cde4d6c19
Nileshlejhro <nilesh.pushkarna@lejhro.com>
2023-10-24 00:58:41 +0530

    Server Commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const app = express();
+app.use(cors());
+app.use(cookieParser());
+
+// Set the port you want to use
+const PORT = process.env.PORT || 3001;
+// for local
+// mongodb://0.0.0.0:27017/taskmanager
+
+// for cloud
+// mongodb+srv://nileshp:Nileshhh@cluster0.cwmozw9.mongodb.net/Cluster0?retryWrites=true&w=majority
+mongoose.connect('mongodb://0.0.0.0:27017/taskmanager', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(bodyParser.json());
+
+// Routes
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/tasks', require('./routes/tasks'));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
